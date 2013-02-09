1ba7f8d97b5b5b583b944c3aacfd4b7455a0cdff
mettoboshi <mettoboshi.1108@gmail.com>
2013-02-09 15:32:37 +0900

    Initial Commit

---
+++ BallThrows/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  BallThrows
+//
+//  Created by mettoboshi on 13/02/09.
+//  Copyright mettobo 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ BallThrows/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BallThrows
+//
+//  Created by mettoboshi on 13/02/09.
+//  Copyright mettobo 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
